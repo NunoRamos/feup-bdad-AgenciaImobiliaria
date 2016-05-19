76d5c0105b76cf990bc761c50fea7f82e64d2886
bernardobelchior1 <bernardo.belchior1@gmail.com>
2016-05-19 19:18:42 +0100

    Updated select.sql

--- select.sql
+++ select.sql
@@ -2,8 +2,10 @@ SELECT nome, montante FROM Cliente JOIN Pagamento
 ON (Cliente.id = Pagamento.destinatarioId)
 ORDER BY montante DESC LIMIT 10;
 
-SELECT AVG(precoArrendamento) FROM Imovel 
-WHERE (anoConstrucao <= 2010 AND precoArrendamento NOT NULL);
+SELECT anoConstrucao, AVG(precoArrendamento) FROM Imovel 
+WHERE (anoConstrucao BETWEEN 2000 AND 2010 AND precoArrendamento NOT NULL)
+GROUP BY anoConstrucao
+ORDER BY AVG(precoArrendamento) DESC;
 
 SELECT nome FROM Cliente JOIN Pagamento
-ON (Cliente.id = Pagamento.destinatarioId AND julianday(dataDevida) < julianday('now') AND dataPagamento IS NULL); 
+ON (Cliente.id = Pagamento.destinatarioId AND strftime('%s', dataDevida) < strftime('%s', 'now') AND dataPagamento IS NULL); 
