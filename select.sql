6d859c34d87f8f595ab4258bd1650d5b900b6a0e
NunoRamos <nunor07.unfc@gmail.com>
2016-05-21 17:52:08 +0100

    Adding subtitles to select.sql

--- select.sql
+++ select.sql
@@ -1,15 +1,19 @@
+--Top 10 Clientes com maiores montantes envolvidos
 SELECT nome, montante FROM Cliente JOIN Pagamento
 ON (Cliente.id = Pagamento.destinatarioId)
 ORDER BY montante DESC LIMIT 10;
 
+--Valor médio da renda dos Imóveis agrupado por ano entre 2000 e 2010 
 SELECT anoConstrucao, AVG(precoArrendamento) FROM Imovel 
 WHERE (anoConstrucao BETWEEN 2000 AND 2010 AND precoArrendamento NOT NULL)
 GROUP BY anoConstrucao
 ORDER BY AVG(precoArrendamento) DESC;
 
+--Clientes com rendas em atraso
 SELECT nome FROM Cliente JOIN Pagamento
 ON (Cliente.id = Pagamento.destinatarioId AND dataDevida < 'now' AND dataPagamento IS NULL); 
 
+--Todos os imóveis com SPA privado no Distrito do Porto
 SELECT Imovel.id, Imovel.morada FROM Spa JOIN 
 	(Imovel JOIN (
 			CodigoPostal JOIN (
@@ -21,36 +25,44 @@ SELECT Imovel.id, Imovel.morada FROM Spa JOIN
 		ON Imovel.codigoPostal = CodigoPostal.codigo)
 	ON Imovel.id = Spa.idImovel;
 
-SELECT Imovel.id, Imovel.descricao, TipoImovel.tipo
-FROM Imovel INNER JOIN TipoImovel ON 
-Imovel.idTipoImovel = TipoImovel.id AND TipoImovel.tipo = 'Quinta' AND Imovel.areaTotal > 2500;
+--Todos os condomínios com mais de 25 lugares de estacionamento e piscina comunitária
+SELECT Condominio.id
+FROM Piscina INNER JOIN (Condominio INNER JOIN Garagem ON Condominio.id = Garagem.idCondominio AND Garagem.lugaresEstacionamento>25) 
+ON Condominio.id = Piscina.idCondominio;
 
+--Todos os imóveis com renda abaixo dos 400€
+SELECT Imovel.id, Imovel.precoArrendamento
+FROM Imovel
+WHERE Imovel.precoArrendamento < 400;
+
+--Top 10 transferências ordenadas pela comissão
+SELECT Pagamento.comissao, Pagamento.montante, Pagamento.comissao*Pagamento.montante
+FROM Pagamento
+ORDER BY Pagamento.comissao*Pagamento.montante DESC LIMIT 10;
+
+--Condomínios com ginásio, sala de eventos e court de ténis
+SELECT Ginasio.idCondominio, Ginasio.id, SalaEventos.id, CourtTenis.id
+FROM Ginasio INNER JOIN ( SalaEventos INNER JOIN CourtTenis ON SalaEventos.idCondominio = CourtTenis.idCondominio)
+ON Ginasio.idCondominio = CourtTenis.idCondominio;
+
+--Imóveis sem condomínio no Concelho da Maia
 SELECT Imovel.id
 FROM Imovel INNER JOIN (Freguesia INNER JOIN CodigoPostal ON Freguesia.id = CodigoPostal.idFreguesia AND Freguesia.nome = 'Maia') 
 ON Imovel.codigoPostal = CodigoPostal.codigo
 WHERE Imovel.idCondominio IS NULL;
 
-SELECT Ginasio.id, SalaEventos.id, CourtTenis.id, Ginasio.idCondominio
-FROM Ginasio INNER JOIN ( SalaEventos INNER JOIN CourtTenis ON SalaEventos.idCondominio = CourtTenis.idCondominio)
-ON Ginasio.idCondominio = CourtTenis.idCondominio;
+--Quinta com área superior a 2500m²
+SELECT Imovel.id, Imovel.descricao, TipoImovel.tipo
+FROM Imovel INNER JOIN TipoImovel ON 
+Imovel.idTipoImovel = TipoImovel.id AND TipoImovel.tipo = 'Quinta' AND Imovel.areaTotal > 2500;
 
-SELECT Pagamento.comissao, Pagamento.montante, Pagamento.comissao*Pagamento.montante
-FROM Pagamento
-ORDER BY Pagamento.comissao*Pagamento.montante DESC LIMIT 10;
-
-SELECT Imovel.id, Imovel.precoArrendamento
-FROM Imovel
-WHERE Imovel.precoArrendamento < 400;
-
-SELECT Condominio.id
-FROM Piscina INNER JOIN (Condominio INNER JOIN Garagem ON Condominio.id = Garagem.idCondominio AND Garagem.lugaresEstacionamento>25) 
-ON Condominio.id = Piscina.idCondominio;
-
+--Melhor imóvel relação custo/área
 SELECT Imovel.id,Imovel.precoVenda/Imovel.areaTotal
 FROM Imovel
 WHERE Imovel.precoVenda/Imovel.areaTotal IS NOT NULL
 ORDER BY Imovel.precoVenda/Imovel.areaTotal DESC;
 
+--Número de imóveis vendidos com foto
 SELECT Pagamento.imovelId
 FROM Pagamento 
 WHERE (SELECT COUNT(*)
@@ -58,16 +70,7 @@ WHERE (SELECT COUNT(*)
 		WHERE Foto.idImovel = Pagamento.imovelId) != 0
 		AND Pagamento.vendaOuArrendamento=0;
 
-SELECT DISTINCT Cliente.nome FROM Cliente JOIN Pagamento
-ON (Cliente.id = Pagamento.remetenteId OR Cliente.id = Pagamento.destinatarioId);
-
-SELECT id, nome, MAX(lucro) FROM (
-	SELECT Cliente.id AS id, Cliente.nome AS nome, SUM(Pagamento.comissao*Pagamento.montante) AS lucro FROM Cliente JOIN Pagamento
-	ON (Cliente.id = Pagamento.remetenteId)
-	GROUP BY Cliente.id
-	ORDER BY lucro DESC
-	LIMIT 1, 1);
-	
+--Distritos ordenados pelo número de imóveis vendidos
 SELECT Distrito.nome
 FROM Distrito INNER JOIN 
 		(Concelho INNER JOIN 
@@ -79,4 +82,16 @@ FROM Distrito INNER JOIN
 WHERE ( SELECT Pagamento.vendaOuArrendamento
 		FROM Pagamento
 		WHERE Imovel.id = Pagamento.imovelId) = 0;
+		
+-- Clientes que já efetuam compras e arrendamentos
+SELECT DISTINCT Cliente.nome FROM Cliente JOIN Pagamento
+ON (Cliente.id = Pagamento.remetenteId OR Cliente.id = Pagamento.destinatarioId);
+
+--Qual segundo cliente que mais lucro deu à agência
+SELECT id, nome, MAX(lucro) FROM (
+	SELECT Cliente.id AS id, Cliente.nome AS nome, SUM(Pagamento.comissao*Pagamento.montante) AS lucro FROM Cliente JOIN Pagamento
+	ON (Cliente.id = Pagamento.remetenteId)
+	GROUP BY Cliente.id
+	ORDER BY lucro DESC
+	LIMIT 1, 1);
 
